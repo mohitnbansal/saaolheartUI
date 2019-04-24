33f9f436500505e09fedf62bdae7ee5ec18ecf85
Mohit Bansal <Mohit.Bansal@gep.com>
2019-04-25 01:33:57 +0530

    logout

--- src/app/services/loader/loader-interceptor.service.ts
+++ src/app/services/loader/loader-interceptor.service.ts
@@ -1,14 +1,16 @@
 import { Observable } from 'rxjs';
 import { LoaderService } from './loader.service';
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpHandler, HttpRequest, HttpEvent, HttpResponse } from '@angular/common/http';
+import { HttpInterceptor, HttpHandler, HttpRequest, HttpEvent, HttpResponse, HttpErrorResponse } from '@angular/common/http';
 import { tap } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoaderInterceptorService implements HttpInterceptor {
-  constructor(private loaderService: LoaderService) { }
+  constructor(private loaderService: LoaderService,
+    public route: Router) { }
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     this.showLoader();
     return next.handle(req).pipe(tap((event: HttpEvent<any>) => { 
@@ -17,6 +19,17 @@ export class LoaderInterceptorService implements HttpInterceptor {
       }
     },
       (err: any) => {
+        if(err instanceof HttpErrorResponse){
+          console.log(err);
+            if(err.error.error === 'invalid_token' && err.status === 401 ){
+              this.onEnd();
+              // const err = [];
+              // err.push('Session Exprired , Kindly Relogin');
+              // this.flashmessage.showRed(err, 4000);
+              this.route.navigateByUrl('/login', { replaceUrl: true });
+            }
+  
+        }
         this.onEnd();
     }));
   }
